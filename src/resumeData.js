b4bf2671c084d1c6168d3b51ca22cb4393cee8a1
nitinAdarsh <69833884+nitinAdarsh@users.noreply.github.com>
2020-09-08 00:23:10 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Nitin Adarsh",
+    "role": "Frontend Developer and Competitive Programmer",
+    "linkedinId":"https://www.linkedin.com/in/nitin-adarsh-a82282152/",
+    "skypeid": NULL,
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nitin-adarsh-a82282152/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nitinAdarsh",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":NULL,
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at The LPU  and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "education":[
+      {
+        "UniversityName":"LPU",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
